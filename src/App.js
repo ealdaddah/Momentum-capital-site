cf7e71f4b9e827cd8c5224238788ee44653b28eb
ealdaddah <e.aldaddah@mtmwealth.com>
2025-04-11 09:22:08 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Momentum Capital Ventures</h1>
+      <p>Website coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
