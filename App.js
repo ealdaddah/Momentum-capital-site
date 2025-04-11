dfd33df72d03b08c5972b45909b563cc54f060b2
ealdaddah <e.aldaddah@mtmwealth.com>
2025-04-11 09:12:09 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Momentum Capital Ventures</h1>
+      <p>Coming Soon</p>
+    </div>
+  );
+}
+
+export default App;
